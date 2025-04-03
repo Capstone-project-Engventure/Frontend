5c1644bb9b3970303ebf8f5a98e9c14110fc932b
Bach IT <87653572+Bach2k2@users.noreply.github.com>
2025-04-03 20:21:27 +0700

    First commit: create base app

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,113 +1,15 @@
 import Image from "next/image";
-import BackgroundImg from "../../public/english_app_background.jpg";
+// import BackgroundImg from "../../../public/english_app_background.jpg";
+const BackgroundImg = "/english_app_background.jpg";
 import { MdOutlineEmail, MdLockOutline } from "react-icons/md";
 import { LuEye, LuEyeClosed } from "react-icons/lu";
 
-
-
-export default function Home() {
+export default function Register() {
   return (
-    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    //   <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-    //     <Image
-    //       className="dark:invert"
-    //       src="/next.svg"
-    //       alt="Next.js logo"
-    //       width={180}
-    //       height={38}
-    //       priority
-    //     />
-    //     <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-    //       <li className="mb-2 tracking-[-.01em]">
-    //         Get started by editing{" "}
-    //         <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-    //           src/app/page.tsx
-    //         </code>
-    //         .
-    //       </li>
-    //       <li className="tracking-[-.01em]">
-    //         Save and see your changes instantly.
-    //       </li>
-    //     </ol>
-
-    //     <div className="flex gap-4 items-center flex-col sm:flex-row">
-    //       <a
-    //         className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-    //         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         <Image
-    //           className="dark:invert"
-    //           src="/vercel.svg"
-    //           alt="Vercel logomark"
-    //           width={20}
-    //           height={20}
-    //         />
-    //         Deploy now
-    //       </a>
-    //       <a
-    //         className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-    //         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         Read our docs
-    //       </a>
-    //     </div>
-    //   </main>
-    //   <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-    //     <a
-    //       className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-    //       href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <Image
-    //         aria-hidden
-    //         src="/file.svg"
-    //         alt="File icon"
-    //         width={16}
-    //         height={16}
-    //       />
-    //       Learn
-    //     </a>
-    //     <a
-    //       className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-    //       href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <Image
-    //         aria-hidden
-    //         src="/window.svg"
-    //         alt="Window icon"
-    //         width={16}
-    //         height={16}
-    //       />
-    //       Examples
-    //     </a>
-    //     <a
-    //       className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-    //       href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <Image
-    //         aria-hidden
-    //         src="/globe.svg"
-    //         alt="Globe icon"
-    //         width={16}
-    //         height={16}
-    //       />
-    //       Go to nextjs.org →
-    //     </a>
-    //   </footer>
-    // </div>
     <div className="w-full min-h-screen">
       <div
         style={{
-          backgroundImage: `url('${BackgroundImg.src}')`,
+          backgroundImage: `url('${BackgroundImg}')`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
@@ -134,8 +36,26 @@ export default function Home() {
 
           <div className="flex flex-col justify-center items-center px-6 py-12 m-auto sm:col-span-5 w-full ">
             <div className="page_login__form opacity-90 w-full p-5 mx-4 mt-10 px-6 py-12  bg-white rounded-2xl min-h-96">
-              <h2 className="text-black font-bold text-center">Login</h2>
+              <h2 className="text-black font-bold text-center">Đăng ký</h2>
               <form className="space-y-4 md:space-y-6">
+                {/* Tên */}
+
+                <div className="gap-2">
+                  <label className="block text-sm text-gray-900">Tên</label>
+                  <div className="mt-2">
+                    <div className="flex items-center border border-gray-300 rounded-lg pl-3">
+                      <div className="shrink-0 text-base text-gray-500 items-center gap-1 border-r-1 pr-1 select-none">
+                        <MdOutlineEmail />
+                      </div>
+                      <input
+                        type="text"
+                        className="block grow py-1.5 pl-1 pr-3 w-full text-base text-gray-900 placeholder:text-gray-400"
+                        placeholder="Nhập tên tại đây"
+                      />
+                    </div>
+                  </div>
+                </div>
+
                 {/*  Email */}
                 <div className="gap-2">
                   <label className="block text-sm text-gray-900">Email</label>
@@ -205,7 +125,9 @@ export default function Home() {
                   Sign in
                 </button>
                 <div className="items-center flex bg-[#a7abc3] justify-center my-5 w-full relative h-0.5">
-                  <span className="font-light inline-flex text-gray-600 bg-white left-0 relative px-2.5 top-0">Hoặc</span>
+                  <span className="font-light inline-flex text-gray-600 bg-white left-0 relative px-2.5 top-0">
+                    Hoặc
+                  </span>
                 </div>
                 <div className="group_btn_login_social flex gap-3.5 items-center justify-center">
                   <button className="btn btn_login_social">Google</button>
@@ -220,7 +142,6 @@ export default function Home() {
                     Sign up
                   </a>
                 </p>
-                
               </form>
             </div>
           </div>
